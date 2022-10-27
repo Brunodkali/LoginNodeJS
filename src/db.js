78a8a432948952dfae2658843bd00bc2328deb57
Bruno Victor Oliveira Duarte <duartebruno581@gmail.com>
2022-10-27 15:07:12 -0300

    Add files via upload

--- src/db.js
+++ src/db.js
@@ -1,22 +1,11 @@
+/*const mongoose = require('mongoose');
 const express = require('express');
-const session = require('express-session');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-
-const port = 3000;
-var path = require('path');
 const app = express();
 
-app.use(session({secret:'batata'}));
-app.use(bodyParser.urlencoded({extended:true}));
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
-app.use('/public', express.static(path.join(__dirname, 'public')));
-app.set('views', path.join(__dirname, '/views'));
-
-app.post('/', (req, res)=> {
+// teste login
+app.post('/login', (req, res)=> {
     try{
-        var login = req.body.login;
+        var name = req.body.login;
         var senha = req.body.senha;
         mongoose.connect('mongodb://127.0.0.1/teste', function(err, db){
             if(err){
@@ -26,15 +15,15 @@ app.post('/', (req, res)=> {
                 if(err){
                     throw err;
                 }
+                console.log(result);
                 var count = Object.keys(result).length;
                 var i=0;
                 for(i = 0; i<count; i++){
                     nomeUsuario = result[i].name;
                     senhaUsuario = result[i].senha;
-                        if(nomeUsuario == login && senhaUsuario == senha){
+                        if(nomeUsuario == name && senhaUsuario == senha){
                             //Logado com sucesso
-                            req.session.logado = login;
-                            res.render('logado');
+                            req.session.login = name;
                         }else{
                             res.render('formulario');
                         }
@@ -47,15 +36,4 @@ app.post('/', (req, res)=> {
     }
 })
 
-app.get('/', (req, res)=>{
-    if (req.session.logado) {
-        res.render('logado');
-    } else {
-        res.render('formulario');
-    }
-});
-
-
-app.listen(port, ()=>{
-    console.log('Servidor rodando');
-});
+module.exports = { app };*/
