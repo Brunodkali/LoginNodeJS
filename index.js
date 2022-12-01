955be99caf1329740bb5ae039ee67b3fef6a7935
Bruno Victor Oliveira Duarte <duartebruno581@gmail.com>
2022-12-01 18:11:30 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,34 +1,27 @@
-// Requires
 const express = require('express');
 const session = require('express-session');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-var path = require('path');
-
-// Variávies globais
 const port = 5000;
 const app = express();
 const db = mongoose.connection;
+var path = require('path');
 
-// Tratamento de conexão com banco de dados
+app.use(session({ secret:'batata' }));
+app.set('view engine', 'ejs');
+app.use('/public', express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, '/views'));
+app.use(bodyParser.urlencoded({extended:true}));
+
 db.on('error', (err)=> console.log(err));
 db.once('open', ()=> console.log('Banco de dados conectado'));
 
-// Uses e sets que serão usados no back-end
-app.use(session({secret:'batata'}));
-app.use(bodyParser.urlencoded({extended:true}));
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
-app.use('/public', express.static(path.join(__dirname, 'public')));
-app.set('views', path.join(__dirname, '/views'));
-
-
-// Tratamento de dados provindos do front e busca no banco de dados
+// Função para tratamento de dados provindos do front e busca no banco de dados
 app.post('/', (req, res)=> {
     try{
-        var login = req.body.login;
-        var senha = req.body.senha;
-        // Conexão com banco
+        let login = req.body.login;
+        let senha = req.body.senha;
+
         mongoose.connect('mongodb://127.0.0.1/teste', function(err, db){
             if(err){
                 throw err;
@@ -37,20 +30,18 @@ app.post('/', (req, res)=> {
                 if(err){
                     throw err;
                 }
-                var count = Object.keys(result).length;
-                var i=0;
-                for(i = 0; i<count; i++){
-                // Conmparação com usuário provindo do front com banco de dados
+                let count = Object.keys(result).length;
+                let i = 0;
+
+                for(i; i < count; i++){
                     nomeUsuario = result[i].name;
                     senhaUsuario = result[i].senha;
+                    
                     if(nomeUsuario == login && senhaUsuario == senha){
-                        //Logado com sucesso
-                        console.log(count);
                         req.session.login = login;
                         res.render('logado');
                     }else{
                         res.render('formulario');
-                        console.log('error ali');
                     }
                     break;
                     }
@@ -62,7 +53,6 @@ app.post('/', (req, res)=> {
     }
 });
 
-// Check se existe Session no front-end ou não
 app.get('/', (req, res)=>{
     if (req.session.login) {
         res.render('logado');
@@ -72,5 +62,5 @@ app.get('/', (req, res)=>{
 });
 
 app.listen(port, ()=>{
-    console.log('Servidor rodando');
+    console.log(`Servidor rodando na porta: ${port}`);
 });
